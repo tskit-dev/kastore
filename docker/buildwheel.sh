9f0ae241e99e180a4fc2ce6b23b34e6bebf0b2de
Ben Jeffery <ben.jeffery@bdi.ox.ac.uk>
2020-06-16 00:09:54 +0100

    Use oldest-supported-numpy in wheel building

--- docker/buildwheel.sh
+++ docker/buildwheel.sh
@@ -11,6 +11,9 @@ cd python
 for V in "${PYTHON_VERSIONS[@]}"; do
     PYBIN=/opt/python/$V/bin
     rm -rf build/       # Avoid lib build by narrow Python is used by wide python
+    # Instead of letting setup.py install a newer numpy we install it here
+    # using the oldest supported version for ABI compatibility
+    $PYBIN/pip install oldest-supported-numpy
     $PYBIN/python setup.py build_ext --inplace
     $PYBIN/python setup.py bdist_wheel
 done
