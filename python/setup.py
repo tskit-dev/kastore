dc776a300209dbf297ee1ae3680b65825a664887
Ben Jeffery <ben.jeffery@bdi.ox.ac.uk>
2024-06-24 10:16:56 +0100

    Upgrade actions infrastructure, numpy2 support, osx ARM wheels.

--- python/setup.py
+++ python/setup.py
@@ -1,9 +1,6 @@
-import os.path
-
 import numpy
 from setuptools import Extension
 from setuptools import setup
-
 
 _kastore_module = Extension(
     "_kastore",
@@ -12,17 +9,6 @@ _kastore_module = Extension(
     include_dirs=["lib", numpy.get_include()],
 )
 
-
-# After exec'ing this file we have kastore_version defined.
-kastore_version = None  # Keep PEP8 happy.
-version_file = os.path.join("kastore", "_version.py")
-with open(version_file) as f:
-    exec(f.read())
-
 setup(
-    # The package name along with all the other metadata is specified in setup.cfg
-    # However, GitHub's dependency graph can't see the package unless we put this here.
-    name="kastore",
-    version=kastore_version,
     ext_modules=[_kastore_module],
 )
